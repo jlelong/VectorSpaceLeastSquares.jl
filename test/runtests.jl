86c03bc5075cc14cf1ca5aa6a6c93f762cd2ba97
Jerome Lelong <jerome.lelong@univ-grenoble-alpes.fr>
2025-10-15 11:30:07 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,23 @@ end
 
 function testPol1d(degree::Integer, x::Real, func1d::Function)
     @assert degree >= 2 "degree must be >= 2"
-    f0 = func1d(x, 0)
     f1 = func1d(x, 1)
-    compeps(func1d(x, degree), func1d(x, degree, 1, f1, f0), 1.E-10)
+    f2 = func1d(x, 2)
+    compeps(func1d(x, degree), func1d(x, degree, 2, f2, f1), 1.E-10)
+end
+
+function evalPolynomial(polType::PolynomialType, degree, nVariates, x::AbstractVector{<:Real})
+    @assert length(x) == nVariates "x must have size nVariates"
+    p = Polynomial(degree, nVariates, polType)
+    fullTensor = Array(p.tensor)
+    for j in 1:size(p)
+        val1 = value(p, x, j)
+        val2 = prod([value(p.type, fullTensor[i, j], x[i]) for i in 1:nVariates])
+        if !compeps(val1, val2, 1E-10)
+            return false
+        end
+    end
+    return true
 end
 
 @testset "Evaluate 1d polynomials" begin
@@ -27,8 +41,27 @@ end
     @test testPol1d(6, 3., VectorSpaceLeastSquares.tchebychev1d)
 end
 
+@testset "Differentiate 1d polynomials" begin
+    @test testPol1d(4, 3., VectorSpaceLeastSquares.dtchebychev1d)
+    @test testPol1d(2, 3., VectorSpaceLeastSquares.dtchebychev1d)
+    @test testPol1d(6, 3., VectorSpaceLeastSquares.dtchebychev1d)
+end
+
 
 @testset "Create multivariate polynomials" begin
     # Write your tests here.
     @test createPolynomial()
+end
+
+@testset "Evaluate multi-variate polynomials" begin
+    degree = 4
+    nVariates = 5
+    x = randn(nVariates)
+    evalPolynomial(Canonic, 4, nVariates, x)
+    evalPolynomial(Hermite, 4, nVariates, x)
+    evalPolynomial(Tchebychev, 4, nVariates, x)
+end
+
+@testset "Differentiate multivariate polynomials" begin
+    
 end
