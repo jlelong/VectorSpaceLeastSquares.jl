6d56afd8704d3e7cdc9fcaa1f0ad913602137a3c
Jerome Lelong <jerome.lelong@univ-grenoble-alpes.fr>
2025-10-16 15:33:27 +0200

    Add generic functions

--- src/basis.jl
+++ src/basis.jl
@@ -2,13 +2,23 @@ using SparseArrays
 import Base: size
 
 abstract type AbstractBasis end
-function nVariates(::AbstractBasis) end
-function size(::AbstractBasis) end
 
-struct LinearTransform
-    scale::Vector{<:Real}
-    center::Vector{<:Real}
-end
+"""
+Return the number of variates of the functions inside the basis
+"""
+function nVariates(B::AbstractBasis) end
+"""
+Return the number of elements in the basis
+"""
+function size(B::AbstractBasis) end
+"""
+Compute the value of the `index`-th basis function at point `x`
+"""
+function value(B::AbstractBasis, x::AbstractVector{<:Real}, index::Integer) end
+"""
+Compute the value of the first derivative of the `index`-th basis function w.r.t to the `derivativeIndex` variate at point `x`
+"""
+function derivative(B::AbstractBasis, x::AbstractVector{<:Real}, index::Integer, derivativeIndex::Integer) end
 
 include("polynomial.jl")
 include("piecewiseconstant.jl")
